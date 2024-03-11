c68e7bde5db42e8bcd324a4d0c1cea0d4b393611
Ryanb999 <aryanbhatt980@gmail.com>
2024-03-11 00:09:50 -0700

    save

---
+++ assets/js/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// Typing Animation
+	new TypeIt('#whatIAm', {
+		strings: [' a High School Student.', ' a Programmer.', ' a NBA Fan.', ' Driven.', 'Passionate.'],
+		breakLines: false,
+		nextStringDelay: 2000,
+		startDelay: 550
+	});
+});
