54c30baa15714d69cf801f27e1ea95dc12fa2a5a
Ryanb999 <aryanbhatt980@gmail.com>
2024-03-18 21:06:25 -0700

    I love max

--- assets/js/script.tsx
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Rotate Squares
-    window.addEventListener('scroll', function() {
-        let scrolled = window.scrollY;
-
-        document.querySelector('.left-square')?.setAttribute('style', `transform: rotate(${20 - (scrolled / 10)}deg)`);
-        document.querySelector('.right-square')?.setAttribute('style', `transform: rotate(${-20 + (scrolled / 10)}deg)`);
-    });
-});
