23e6bab0378492065c06d63ca5927ba5015c24df
Ryanb999 <aryanbhatt980@gmail.com>
2024-03-18 21:07:47 -0700

    save

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	// Typing Animation
 	new TypeIt('#whatIAm', {
-		strings: [' a High School Student.', ' a Programmer.', ' a NBA Fan.', ' Driven.', 'Passionate.'],
+		strings: [' a High School Student.', ' a Programmer.', ' a NBA Fan.', ' Driven.', ' Passionate.'],
 		breakLines: false,
 		nextStringDelay: 2000,
 		startDelay: 550
